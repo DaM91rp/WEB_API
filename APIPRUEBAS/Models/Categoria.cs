2a6590c8779a040ee737eb81f63190db35e3edce
DaM91rp <david_rioja_paz@outlook.es>
2022-07-16 02:31:25 -0500

    Creacion de una web api para consumirla

---
+++ APIPRUEBAS/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace APIPRUEBAS.Models
+{
+    public partial class Categoria
+    {
+        public Categoria()
+        {
+            Productos = new HashSet<Producto>();
+        }
+
+        public int IdCategoria { get; set; }
+        public string? Descripcion { get; set; }
+
+        //Con JsonIgnore, al momento de enviar los datos, el campo de producto se ignora para mejorar la vista en lo que son referencias ciclicas
+        [JsonIgnore]
+        public virtual ICollection<Producto> Productos { get; set; }
+    }
+}
